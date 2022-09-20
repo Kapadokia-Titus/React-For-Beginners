04fa85920c39735ec3b34548fabf7f9bf2fab6b2
kapadokia <ktnyandoch@gmail.com>
2022-09-20 05:12:55 +0300

    simple jsx implemetation

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // a simple way of implementing react 
-ReactDOM.render(<h1>Hello Everyone!</h1>, document.getElementById('root'))
+const page = (
+   <div>
+    <h1>Some Fancy title</h1>
+     <p>The description of the fancy title</p>
+   </div>
+)
+
+console.log(page);
+
+ReactDOM.render(page, document.getElementById('root'))
